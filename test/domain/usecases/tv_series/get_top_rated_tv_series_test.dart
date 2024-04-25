29b9ed154584a72445a3fc647ea1079e99f7817e
Tinesh Roy <tinesh12.roy@gmail.com>
2024-04-25 19:40:47 +0700

    flutter test domain usecase tv series

--- test/domain/usecases/tv_series/get_top_rated_tv_series_test.dart
+++ test/domain/usecases/tv_series/get_top_rated_tv_series_test.dart
@@ -1,4 +1,7 @@
+import 'package:dartz/dartz.dart';
 import 'package:ditonton_app/domain/domain.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
 
 import '../../../helpers/test_helper.mocks.dart';
 
@@ -8,16 +11,16 @@ void main() {
 
   setUp(() {
     mockTvSeriesRepository = MockTvSeriesRepository();
-    usecase = GetPopularTvSeries(mockTvSeriesRepository);
+    usecase = GetTopRatedTvSeries(mockTvSeriesRepository);
   });
   final tTvSeries = <TvSeries>[];
 
-  group('GetPopularTvSeries Tests execute', () {
+  group('GetTopRatedTvSeries Tests execute', () {
     test(
         'should get list of tv series from the repository  when execute function is called',
         () async {
       // arrange
-      when(mockTvSeriesRepository.getPopularTvSeries())
+      when(mockTvSeriesRepository.getTopRatedTvSeries())
           .thenAnswer((_) async => Right(tTvSeries));
       // act
       final result = await usecase.execute();
--- test/dummy_data/dummy_objects.dart
+++ test/dummy_data/dummy_objects.dart
@@ -17,8 +17,26 @@ final testMovie = Movie(
   voteAverage: 7.2,
   voteCount: 13507,
 );
+const testTvSeries = TvSeries(
+  backdropPath: '/mUkuc2wyV9dHLG0D0Loaw5pO2s8.jpg',
+  genreIds: [10765, 10759, 18],
+  id: 1399,
+  overview:
+      "Seven noble families fight for control of the mythical land of Westeros. Friction between the houses leads to full-scale war. All while a very ancient evil awakens in the farthest north. Amidst the war, a neglected military order of misfits, the Night's Watch, is all that stands between the realms of men and icy horrors beyond.",
+  popularity: 29.780826,
+  posterPath: '/jIhL6mlT7AblhbHJgEoiBIOUVl1.jpg',
+  voteAverage: 7.91,
+  voteCount: 1172,
+  firstAirDate: '2011-04-17',
+  originCountry: ["US"],
+  originalLanguage: 'en',
+  name: 'Game of Thrones',
+  originalName: 'Game of Thrones',
+);
 
 final testMovieList = [testMovie];
+
+final testTvSeriesList = [testTvSeries];
 
 const testMovieDetail = MovieDetail(
   adult: false,
